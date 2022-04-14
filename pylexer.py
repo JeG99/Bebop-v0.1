d67ddf501344b9da5e78375b209b886ef06f2401
Elias Garza <elias.garza@kalypso.com>
2022-04-13 23:35:47 -0500

    clean

--- pylexer.py
+++ pylexer.py
@@ -1,55 +1,95 @@
+####   Library Importing
 import ply.lex as lex
 
+####   Token Array Definition
 tokens = [
-    'PLUS',
-    'MINUS',
-    'DIVIDE',
-    'TIMES',
-    'EQUALS',
-    'GTHAN',
-    'LTHAN',
-    'DIFFERENT',
     'ID',
-    'COMMA',
-    'STRING',
-    'LPAREN',
-    'RPAREN',
+    'SEMICOLON',
     'LBRACKET',
     'RBRACKET',
-    'SEMICOLON',
+    'COMMA',
+    'LPAREN',
+    'RPAREN',
+    'LSQRBRACKET',
+    'RSQRBRACKET',
+    'ARROW',
     'COLON',
-    'INT_CTE',
-    'FLOAT_CTE'
+    'EQUALS',
+    'READ',
+    'WRITE',
+    'OR',
+    'AND',
+    'NOT',
+    'LTHAN',
+    'GTHAN',
+    'DIFFERENT',
+    'EQUIVALENT',
+    'PLUS',
+    'MINUS',
+    'MULTIPLY',
+    'DIVIDE',
+    'POWER',
+    'SQRT',
+    'DOT',
+    'CONST_INT',
+    'CONST_FLOAT',
+    'CONST_STRING',
+    'CONST_BOOL'
 ]
 
+####   Reserved Dictionary Definition
+
 reserved = {
-    'program': 'PROGRAM',
-    'if':'IF',
-    'else': 'ELSE',
-    'print': 'PRINT',
-    'var': 'VAR',
+    'routine' : 'ROUTINE',
+    'class' : 'CLASS',
     'int': 'INT',
-    'float': 'FLOAT'
+    'float': 'FLOAT',
+    'string' : 'STRING',
+    'bool' : 'BOOL',
+    'construct' : 'CONSTRUCT',
+    'private' : 'PRIVATE',
+    'public' : 'PUBLIC',
+    'def' : 'DEF',
+    'return' : 'RETURN',
+    'new': 'NEW',
+    'while' : 'WHILE',
+    'if' : 'IF',
+    'else' : 'ELSE',
+    'void' : 'VOID',
+    'main' : 'MAIN'
 }
 
 tokens += list(reserved.values())
 
-t_PLUS = r'\+'
-t_MINUS = r'\-'
-t_TIMES = r'\*'
-t_DIVIDE = r'\/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_COMMA = r','
 t_SEMICOLON = r';'
-t_COLON = r':'
 t_LBRACKET = r'\{'
 t_RBRACKET = r'\}'
-t_GTHAN = r'>'
-t_LTHAN = r'<'
+t_COMMA = r','
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LSQRBRACKET = r'\['
+t_RSQRBRACKET = r'\]'
+t_ARROW = r'->'
+t_COLON = r':'
 t_EQUALS = r'='
+t_WRITE = r'<<<'
+t_READ = r'>>>'
+t_OR = r'\|\|'
+t_AND = r'&&'
+t_NOT = r'\!'
+t_LTHAN = r'\<'
+t_GTHAN = r'\>'
 t_DIFFERENT = r'<>'
-t_STRING = r'\"(\"\"|[^\"$])*\"'
+t_EQUIVALENT = r'=='
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_MULTIPLY = r'\*'
+t_DIVIDE = r'\/'
+t_POWER = r'\*\*'
+t_SQRT = r'\|/'
+t_DOT = r'\.'
+t_CONST_BOOL = r'true|false'
+t_CONST_STRING = r'\"(\"\"|[^\"$])*\"'
 
 t_ignore = ' \t'
 
@@ -58,12 +98,12 @@ def t_ID(t):
     t.type = reserved.get(t.value,'ID')
     return t
 
-def t_FLOAT_CTE(t):
+def t_CONST_FLOAT(t):
     r'\d*\.\d+'
     t.value = float(t.value)
     return t
 
-def t_INT_CTE(t):
+def t_CONST_INT(t):
     r'\d+'
     t.value = int(t.value)
     return t
@@ -73,7 +113,6 @@ def t_newline(t):
     t.lexer.lineno += len(t.value)
 
 def t_error(t):
-    #print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
 lexer = lex.lex()
